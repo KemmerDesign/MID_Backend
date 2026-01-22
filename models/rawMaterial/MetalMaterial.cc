6e0d36513ae23b24d5a6413175fd9780ed37216f
Kemmer Torres <artset.kemmer@gmail.com>
2026-01-21 20:18:24 -0500

    TODO : Finish to implement all methods os MetalProfile.cc Class

--- models/rawMaterial/MetalMaterial.cc
+++ models/rawMaterial/MetalMaterial.cc
@@ -1,2 +1,61 @@
 #include "MetalMaterial.h"
 
+namespace rawMaterial{
+    //Constructor y setters privados y publicos
+    MetalProfile::MetalProfile(double lengthMM, double widthMM, double heightMM, double thicknessMM, GeometryType geometry, Materialtype material, double density)
+        : lengthMM(lengthMM), widthMM(widthMM), heightMM(heightMM), thicknessMM(thicknessMM), geometry(geometry), material(material), density(density)
+    {
+        // Calcular area y peso al momento de la creacion
+        double area = 0.0;
+        double weight = 0.0;
+
+        switch (geometry)
+        {
+        case GeometryType::CYLINDRICAL:
+            area = 3.1416 * thicknessMM * lengthMM; // Area lateral del cilindro
+            weight = area * density; // Peso = Area * Densidad
+            break;
+        case GeometryType::RECTANGULAR:
+            area = 2 * (widthMM * heightMM + widthMM * lengthMM + heightMM * lengthMM);
+            weight = area * density;
+            break;
+        case GeometryType::SQUARED:
+            area = 4 * (thicknessMM * lengthMM + thicknessMM * heightMM);
+            weight = area * density;
+            break;
+        default:
+            break;
+        }
+
+        setAreaMM2(area);
+        setWeightGrams(weight);
+    };
+    void MetalProfile::setWeightGrams(double weight){
+        this->weightGrams = weight;
+    };
+    void MetalProfile::setAreaMM2(double area){
+        this->areaMM2 = area;
+    };
+    // TODO: Implementacion de los Setters
+    // TODO: Factory method
+    // Implementacion de los Getters
+    double MetalProfile::getLengthMM() const {
+        return lengthMM;
+    } 
+    double MetalProfile::getWidthMM() const {
+        return widthMM;
+    }
+    double MetalProfile::getHeightMM() const {
+        return heightMM;
+    }
+    double MetalProfile::getThicknessMM() const {
+        return thicknessMM;
+    }
+    double MetalProfile::getWeightGrams() const {
+        return weightGrams;
+    }
+    double MetalProfile::getAreaMM2() const {
+        return areaMM2;
+    }  
+    // TODO: Implementacion serializacion y deserializacion 
+}
--- models/rawMaterial/MetalMaterial.h
+++ models/rawMaterial/MetalMaterial.h
@@ -1,50 +1,72 @@
 #pragma once
 #include <optional>
+#include <nlohmann/json.hpp>
 
-//TODO : Hacer la implementacion para la la serializacion y descerializacion de los objetos de esta clase
-//TODO : Hacer mas universalesta clase, para que soporte diferentes tipos de materiales y no solo acero al carbono
+// TODO : Hacer la implementacion para la la serializacion y descerializacion de los objetos de esta clase
+// TODO : Hacer mas universalesta clase, para que soporte diferentes tipos de materiales y no solo acero al carbono
 namespace rawMaterial
 {
     // Declaracion de constantes para operar internamente
     // Las clases deben estar pensadas para operar en mm, es una condicion clave para que sea el mismo lenguaje.
-    constexpr double INCH_TO_MM = 25.4;
-    constexpr double densityCR = 7.85/1000000; // mm3 * gramos
 
     enum class Materialtype
     {
-        ColdRolled_Steel, // Carbono Regular
-        HotRolled_Steel,  // Alta Resistencia
-        Aluminum,
-        Stanless_Steel
+        COLD_ROLLED_STEEL, // Carbono Regular
+        HOT_ROLLED_STEEL,  // Alta Resistencia
+        ALUMINIUM,
+        STAINLESS_STEEL
+    };
+
+    enum class GeometryType
+    {
+        CYLINDRICAL,
+        RECTANGULAR,
+        SQUARED
     };
 
     class MetalProfile
     {
     private:
-        //Atributos
+        // Atributos
         double lengthMM;
-        double outerDiameterMM;
+        double widthMM;
+        double heightMM;
         double thicknessMM;
-        bool isSquared;
-        bool isRectangular;
-        bool isCylindrical;
-        bool isCR;
-        bool isHR;
-        //Metodos privados
-        MetalProfile(double lengthMM, double outerDiameterMM, double thicknessMM, bool isSquared, bool isRectangular, bool isCylindrical,  bool isCR, bool isHR);
+        double weightGrams;
+        double areaMM2;
+        // Atributos derivados
+        GeometryType geometry;
+        Materialtype material;
+        double density;
+        // Metodos privados
+        MetalProfile(double lengthMM, double widthMM, double heightMM, double thicknessMM, GeometryType geometry, Materialtype material, double density);
+        void setWeightGrams(double weight);
+        void setAreaMM2(double area);
     public:
-        MetalProfile();
-        //Este constructor es para crear tubos de acero al carbono, se usa el patron Factory Method ya que es mas seguro
-        //adicional, si un insumo se va a crear solo puede tener un tipo de geometria, y un tipo de insumo de material.
-        //si entraran parametros invalidos, se retornaria un std::nullopt
-        std::optional<MetalProfile> static pipeCR_HR_Constructor(double lengthMM, double outerDiameterMM, double thicknessMM, bool isSquared, bool isRectangular, bool isCylindrical);        double getLengthMM() const;
-        double getOuterDiameterMM() const;
+        MetalProfile() = default;
+        static std::optional<MetalProfile> createMetalProfile(double lengthMM, double widthMM, double heightMM, double thicknessMM, GeometryType geometry, Materialtype material, double density);
+        // Getters
+        double getLengthMM() const;
+        double getWidthMM() const;
+        double getHeightMM() const;
         double getThicknessMM() const;
-        double getInnerDiameterMM() const;
-        double calculateVolumeMM3() const;
-        double calculateWeightGrams() const;
-        bool getIsSquared() const;
-        bool getIsRectangular() const;
-        bool getIsCylindrical() const;
+        double getWeightGrams() const;
+        double getAreaMM2() const;
+        GeometryType getGeometry() const;
+        Materialtype getMaterial() const;
+        double getDensity() const;
+
+        // Setters
+        void setLengthMM(double length);
+        void setWidthMM(double width);
+        void setHeightMM(double height);
+        void setThicknessMM(double thickness);
+        void setGeometry(GeometryType geometry);
+        void setMaterial(Materialtype material);
+        void setDensity(double density);
+
+        // Serialization
+        nlohmann::json toJson() const;
+        static std::optional<MetalProfile> fromJson(const nlohmann::json& jsonObj);
     };
 }
